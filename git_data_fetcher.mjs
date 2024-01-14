07a839b1851f7a0e7806060c4c49d79532992bf7
vjspranav <pranavasri@live.in>
2024-01-14 17:21:37 +0530

    Update PRs and issues * Remove all the dummy issues * improvise script to fetch git data

--- git_data_fetcher.mjs
+++ git_data_fetcher.mjs
@@ -9,298 +9,296 @@ const openSource = {
   githubUserName: process.env.GITHUB_USERNAME,
 };
 
-const query_pr = {
-  query: `
-	query {
-	  user(login: "${openSource.githubUserName}"){
-	    pullRequests(last: 100, orderBy: {field: CREATED_AT, direction: DESC}){
-      totalCount
-      nodes{
-        id
-        title
-        url
-        state
-	      mergedBy {
-	          avatarUrl
-	          url
-	          login
-	      }
-	      createdAt
-	      number
-        changedFiles
-	      additions
-	      deletions
-        baseRepository {
-	          name
-	          url
-	          owner {
-	            avatarUrl
-	            login
-	            url
-	          }
-	        }
-      }
-    }
-	}
-}
-	`,
-};
-
-const query_issue = {
-  query: `query{
-
-		user(login: "${openSource.githubUserName}") {
-    issues(last: 100, orderBy: {field:CREATED_AT, direction: DESC}){
-      totalCount
-      nodes{
-      	id
-        closed
-        title
-        createdAt
-        url
-        number
-        assignees(first:100){
-          nodes{
+const QUERY_PR = `
+  query {
+    user(login: "${openSource.githubUserName}") {
+      pullRequests(last: 100, orderBy: {field: CREATED_AT, direction: DESC}) {
+        totalCount
+        nodes {
+          id
+          title
+          url
+          state
+          mergedBy {
             avatarUrl
+            url
+            login
+          }
+          createdAt
+          number
+          changedFiles
+          additions
+          deletions
+          baseRepository {
             name
             url
-          }
-        }
-        repository{
-          name
-          url
-          owner{
-            login
-            avatarUrl
-            url
+            owner {
+              avatarUrl
+              login
+              url
+            }
           }
         }
       }
     }
   }
+`;
 
-	}`,
-};
+const IGNORED_REPO = "TestIssuesRepo"; // Name of the repo to ignore
+const MAX_ISSUES_COUNT = 100; // Maximum number of issues to fetch
 
-const query_org = {
-  query: `query{
-	user(login: "${openSource.githubUserName}") {
-	    repositoriesContributedTo(last: 100){
-	      totalCount
-	      nodes{
-	        owner{
-	          login
-	          avatarUrl
-	          __typename
-	        }
-	      }
-	    }
-	  }
-	}`,
-};
+const QUERY_ISSUE = (afterCursor) => `
+  query {
+    user(login: "${openSource.githubUserName}") {
+      issues(last: 100, after: ${afterCursor}, orderBy: {field: CREATED_AT, direction: DESC}) {
+        totalCount
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+        nodes {
+          id
+          closed
+          title
+          createdAt
+          url
+          number
+          assignees(first:100){
+            nodes{
+              avatarUrl
+              name
+              url
+            }
+          }
+          repository {
+            name
+            url
+            owner {
+              login
+              avatarUrl
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
-const query_pinned_projects = {
-  query: `
-	query { 
-	  user(login: "${openSource.githubUserName}") { 
-	    pinnedItems(first: 6, types: REPOSITORY) {
-	      totalCount
-	      nodes{
-	        ... on Repository{
-	          id
-		          name
-		          createdAt,
-		          url,
-		          description,
-		          isFork,
-		          languages(first:10){
-		            nodes{
-		              name
-		            }
-		          }
-	        }
-	      }
-		  }
-	  }
-	}
-	`,
-};
+const QUERY_ORG = `
+  query {
+    user(login: "${openSource.githubUserName}") {
+      repositoriesContributedTo(last: 100) {
+        totalCount
+        nodes {
+          owner {
+            login
+            avatarUrl
+            __typename
+          }
+        }
+      }
+    }
+  }
+`;
+
+const QUERY_PINNED_PROJECTS = `
+  query { 
+    user(login: "${openSource.githubUserName}") { 
+      pinnedItems(first: 6, types: REPOSITORY) {
+        totalCount
+        nodes {
+          ... on Repository {
+            id
+            name
+            createdAt
+            url
+            description
+            isFork
+            languages(first: 10) {
+              nodes {
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 const baseUrl = "https://api.github.com/graphql";
-
 const headers = {
   "Content-Type": "application/json",
   Authorization: "bearer " + openSource.githubConvertedToken,
 };
 
-fetch(baseUrl, {
-  method: "POST",
-  headers: headers,
-  body: JSON.stringify(query_pr),
-})
-  .then((response) => response.text())
-  .then((txt) => {
-    const data = JSON.parse(txt);
-    var cropped = { data: [] };
-    cropped["data"] = data["data"]["user"]["pullRequests"]["nodes"];
+async function fetchData(query) {
+  try {
+    const response = await fetch(baseUrl, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify({ query }),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}
 
-    var open = 0;
-    var closed = 0;
-    var merged = 0;
-    for (var i = 0; i < cropped["data"].length; i++) {
-      if (cropped["data"][i]["state"] === "OPEN") open++;
-      else if (cropped["data"][i]["state"] === "MERGED") merged++;
-      else closed++;
-    }
+async function processPRs(data) {
+  const prs = data.data.user.pullRequests.nodes;
+  let open = 0,
+    closed = 0,
+    merged = 0;
 
-    cropped["open"] = open;
-    cropped["closed"] = closed;
-    cropped["merged"] = merged;
-    cropped["totalCount"] = cropped["data"].length;
+  prs.forEach((pr) => {
+    if (pr.state === "OPEN") open++;
+    else if (pr.state === "MERGED") merged++;
+    else closed++;
+  });
 
-    console.log("Fetching the Pull Request Data.\n");
-    fs.writeFile(
-      "./src/shared/opensource/pull_requests.json",
-      JSON.stringify(cropped),
-      function (err) {
-        if (err) {
-          console.log(err);
-        }
-      }
+  return {
+    data: prs,
+    open,
+    closed,
+    merged,
+    totalCount: prs.length,
+  };
+}
+
+async function processIssues() {
+  let issues = [];
+  let hasNextPage = true;
+  let afterCursor = null;
+
+  while (issues.length < MAX_ISSUES_COUNT && hasNextPage) {
+    const query = QUERY_ISSUE(afterCursor);
+    const data = await fetchData(query);
+    const fetchedIssues = data.data.user.issues.nodes;
+    const pageInfo = data.data.user.issues.pageInfo;
+
+    // Filter and add issues
+    const filteredIssues = fetchedIssues.filter(
+      (issue) => issue.repository.name !== IGNORED_REPO
     );
-  })
-  .catch((error) => console.log(JSON.stringify(error)));
+    issues = [...issues, ...filteredIssues].slice(0, MAX_ISSUES_COUNT);
 
-fetch(baseUrl, {
-  method: "POST",
-  headers: headers,
-  body: JSON.stringify(query_issue),
-})
-  .then((response) => response.text())
-  .then((txt) => {
-    const data = JSON.parse(txt);
-    var cropped = { data: [] };
-    cropped["data"] = data["data"]["user"]["issues"]["nodes"];
+    // Update pagination info
+    hasNextPage = pageInfo.hasNextPage;
+    afterCursor = pageInfo.endCursor;
+  }
 
-    var open = 0;
-    var closed = 0;
-    for (var i = 0; i < cropped["data"].length; i++) {
-      if (cropped["data"][i]["closed"] === false) open++;
-      else closed++;
+  // Process the issues count
+  let open = issues.filter((issue) => !issue.closed).length;
+  let closed = issues.length - open;
+
+  return {
+    data: issues,
+    open,
+    closed,
+    totalCount: issues.length,
+  };
+}
+
+async function processOrgs(data) {
+  const orgs = data.data.user.repositoriesContributedTo.nodes;
+  const uniqueOrgs = orgs.reduce((acc, org) => {
+    if (
+      org.owner.__typename === "Organization" &&
+      !acc.some((o) => o.login === org.owner.login)
+    ) {
+      acc.push(org.owner);
     }
+    return acc;
+  }, []);
 
-    cropped["open"] = open;
-    cropped["closed"] = closed;
-    cropped["totalCount"] = cropped["data"].length;
+  return { data: uniqueOrgs };
+}
 
-    console.log("Fetching the Issues Data.\n");
-    fs.writeFile(
-      "./src/shared/opensource/issues.json",
-      JSON.stringify(cropped),
-      function (err) {
-        if (err) {
-          console.log(err);
-        }
-      }
-    );
-  })
-  .catch((error) => console.log(JSON.stringify(error)));
+async function processPinnedProjects(data) {
+  const projects = data.data.user.pinnedItems.nodes;
+  const languages_icons = {
+    Python: "logos-python",
+    "Jupyter Notebook": "logos-jupyter",
+    HTML: "logos-html-5",
+    CSS: "logos-css-3",
+    JavaScript: "logos-javascript",
+    "C#": "logos-c-sharp",
+    Java: "logos-java",
+    Shell: "simple-icons:shell",
+    Ruby: "logos:ruby",
+    PHP: "logos-php",
+    Dockerfile: "simple-icons:docker",
+    Rust: "logos-rust",
+  };
 
-fetch(baseUrl, {
-  method: "POST",
-  headers: headers,
-  body: JSON.stringify(query_org),
-})
-  .then((response) => response.text())
-  .then((txt) => {
-    const data = JSON.parse(txt);
-    const orgs = data["data"]["user"]["repositoriesContributedTo"]["nodes"];
-    var newOrgs = { data: [] };
-    for (var i = 0; i < orgs.length; i++) {
-      var obj = orgs[i]["owner"];
-      if (obj["__typename"] === "Organization") {
-        var flag = 0;
-        for (var j = 0; j < newOrgs["data"].length; j++) {
-          if (JSON.stringify(obj) === JSON.stringify(newOrgs["data"][j])) {
-            flag = 1;
-            break;
-          }
-        }
-        if (flag === 0) {
-          newOrgs["data"].push(obj);
-        }
-      }
+  const processedProjects = projects.map((project) => {
+    const languages = project.languages.nodes.map((lang) => ({
+      name: lang.name,
+      iconifyClass: languages_icons[lang.name] || "",
+    }));
+
+    return { ...project, languages };
+  });
+
+  return { data: processedProjects };
+}
+
+async function writeToFile(fileName, content) {
+  fs.writeFile(fileName, JSON.stringify(content), (err) => {
+    if (err) {
+      console.error("Error writing to file:", fileName, err);
+      throw err;
     }
+    console.log(`Data written to ${fileName}`);
+  });
+}
 
-    console.log("Fetching the Contributed Organization Data.\n");
-    fs.writeFile(
-      "./src/shared/opensource/organizations.json",
-      JSON.stringify(newOrgs),
-      function (err) {
-        if (err) {
-          console.log(err);
-        }
-      }
-    );
-  })
-  .catch((error) => console.log(JSON.stringify(error)));
+async function updateComponent(component) {
+  let query;
+  switch (component) {
+    case "PR":
+      query = QUERY_PR;
+      break;
+    case "Issue":
+      query = QUERY_ISSUE(null);
+      break;
+    case "Org":
+      query = QUERY_ORG;
+      break;
+    case "PinnedProjects":
+      query = QUERY_PINNED_PROJECTS;
+      break;
+    default:
+      throw new Error("Invalid component type");
+  }
 
-const languages_icons = {
-  Python: "logos-python",
-  "Jupyter Notebook": "logos-jupyter",
-  HTML: "logos-html-5",
-  CSS: "logos-css-3",
-  JavaScript: "logos-javascript",
-  "C#": "logos-c-sharp",
-  Java: "logos-java",
-  Shell: "simple-icons:shell",
-  Ruby: "logos:ruby",
-  PHP: "logos-php",
-  Dockerfile: "simple-icons:docker",
-  Rust: "logos-rust",
-};
+  const data = await fetchData(query);
+  switch (component) {
+    case "PR":
+      const prData = await processPRs(data);
+      await writeToFile("./src/shared/opensource/pull_requests.json", prData);
+      break;
+    case "Issue":
+      const issuesData = await processIssues();
+      await writeToFile("./src/shared/opensource/issues.json", issuesData);
+      break;
+    case "Org":
+      const orgData = await processOrgs(data);
+      await writeToFile("./src/shared/opensource/organizations.json", data);
+      break;
+    case "PinnedProjects":
+      const pinnedProjectsData = await processPinnedProjects(data);
+      await writeToFile("./src/shared/opensource/projects.json", data);
+      break;
+  }
+}
 
-fetch(baseUrl, {
-  method: "POST",
-  headers: headers,
-  body: JSON.stringify(query_pinned_projects),
-})
-  .then((response) => response.text())
-  .then((txt) => {
-    const data = JSON.parse(txt);
-    // console.log(txt);
-    const projects = data["data"]["user"]["pinnedItems"]["nodes"];
-    var newProjects = { data: [] };
-    for (var i = 0; i < projects.length; i++) {
-      var obj = projects[i];
-      var langobjs = obj["languages"]["nodes"];
-      var newLangobjs = [];
-      for (var j = 0; j < langobjs.length; j++) {
-        if (langobjs[j]["name"] in languages_icons) {
-          newLangobjs.push({
-            name: langobjs[j]["name"],
-            iconifyClass: languages_icons[langobjs[j]["name"]],
-          });
-        }
-      }
-      obj["languages"] = newLangobjs;
-      newProjects["data"].push(obj);
-    }
-
-    console.log("Fetching the Pinned Projects Data.\n");
-    fs.writeFile(
-      "./src/shared/opensource/projects.json",
-      JSON.stringify(newProjects),
-      function (err) {
-        if (err) {
-          console.log(
-            "Error occured in pinned projects 1",
-            JSON.stringify(err)
-          );
-        }
-      }
-    );
-  })
-  .catch((error) =>
-    console.log("Error occured in pinned projects 2", JSON.stringify(error))
-  );
+// Usage example
+updateComponent("PR").catch((err) => console.error(err));
+updateComponent("Issue").catch((err) => console.error(err));
+updateComponent("Org").catch((err) => console.error(err));
+updateComponent("PinnedProjects").catch((err) => console.error(err));
