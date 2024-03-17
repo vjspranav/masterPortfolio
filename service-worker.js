673ee99ba6dde6d58c1d66f423e9e6b966f63ae8
vjspranav <pranavasri@live.in>
2024-03-17 07:26:59 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0139f8e41d5edb5d128e592f216c1cd8.js"
+  "./precache-manifest.a341ec27933853c29e443a9744d1900f.js"
 );
 
 self.addEventListener('message', (event) => {
